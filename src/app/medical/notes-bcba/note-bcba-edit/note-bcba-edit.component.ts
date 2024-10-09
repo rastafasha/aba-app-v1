cecc5f27b07400fb32d06a71d993e53ba4bff8f7
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-10-08 23:06:08 -0300

    AI Summary

--- src/app/medical/notes-bcba/note-bcba-edit/note-bcba-edit.component.ts
+++ src/app/medical/notes-bcba/note-bcba-edit/note-bcba-edit.component.ts
@@ -17,6 +17,8 @@ import { Location } from '@angular/common';
 })
 export class NoteBcbaEditComponent {
   public routes = routes;
+  public summary_note: string = '';
+  public isGeneratingSummary = false;
 
   valid_form:boolean = false;
   valid_form_success:boolean = false;
@@ -46,11 +48,11 @@ export class NoteBcbaEditComponent {
   note_selected:any;
   bip_id:any;
   user:any;
-  
+
   public first_name:string = '';
   public last_name:string = '';
   public diagnosis_code:string = '';
-  
+
   public provider_name_g:string = '';
   public provider_credential:string = '';
   public pos:string = '';
@@ -62,7 +64,7 @@ export class NoteBcbaEditComponent {
   public session_length_total:string = '';
   public session_length_total2:string = '';
   public environmental_changes:string = '';
-  
+
   public sumary_note:string = '';
   public meet_with_client_at:string = '';
   public client_appeared:string = '';
@@ -84,8 +86,8 @@ export class NoteBcbaEditComponent {
   public interventions:any;
   public provider_signature:any;
   public supervisor_signature:any;
-  
-  
+
+
   public pairing:any;
   public response_block:any;
   public DRA:any;
@@ -151,7 +153,7 @@ export class NoteBcbaEditComponent {
   services:any ;
   insurer_id:any ;
 
-  
+
 
   constructor(
     public bipService:BipService,
@@ -166,7 +168,7 @@ export class NoteBcbaEditComponent {
   ){}
 
   ngOnInit(): void {
-    
+
     // window.scrollTo(0, 0);
     this.ativatedRoute.params.subscribe((resp:any)=>{
       this.note_id = resp.id;
@@ -178,14 +180,14 @@ export class NoteBcbaEditComponent {
     this.user = JSON.parse(USER ? USER: '');
     this.doctor_id = this.user.id;
 
-    
+
   }
 
   goBack() {
     this.locations.back(); // <-- go back to previous location on cancel
   }
 
- 
+
   getConfig(){
     this.noteBcbaService.listConfigNote().subscribe((resp:any)=>{
       console.log(resp);
@@ -194,15 +196,15 @@ export class NoteBcbaEditComponent {
       this.roles_bcba = resp.roles_bcba;
       this.hours_days = resp.hours;
       this.specialists = resp.specialists;
-      
+
     })
   }
 
-  
+
 
   getNote(){
     this.noteBcbaService.getNote(this.note_id).subscribe((resp:any)=>{
-      console.log(resp);
+      console.log('respuesta de getNote', resp);
       this.note_selected = resp.noteBcba;
       this.note_selectedId = resp.noteBcba.id;
       this.patient_id = this.note_selected.patient_id;
@@ -210,6 +212,8 @@ export class NoteBcbaEditComponent {
       this.location = this.note_selected.location;
       // this.birth_date = this.note_selected.birth_date;
       this.birth_date = this.note_selected.birth_date ? new Date(this.note_selected.birth_date).toISOString(): '';
+
+      this.summary_note = resp.noteBcba.summary_note || '';
 
       this.provider_credential = this.note_selected.provider_credential;
       this.as_evidenced_by = this.note_selected.as_evidenced_by;
@@ -220,14 +224,14 @@ export class NoteBcbaEditComponent {
       this.note_description = this.note_selected.note_description;
       this.client_response_to_treatment_this_session = this.note_selected.client_response_to_treatment_this_session;
       this.pos = this.note_selected.pos;
-      
-      
+
+
       this.caregivers_training_goalsgroup = resp.caregiver_goals;
       let jsonObj = JSON.parse(this.caregivers_training_goalsgroup) || '';
       this.caregivers_training_goals = jsonObj;
       // console.log(this.caregivers_training_goals);
 
-      
+
       this.rbt_training_goalsgroup = resp.rbt_training_goals;
       let jsonObj1 = JSON.parse(this.rbt_training_goalsgroup) || '';
       this.rbt_training_goals = jsonObj1;
@@ -241,12 +245,12 @@ export class NoteBcbaEditComponent {
       this.selectedValueBCBA = this.note_selected.supervisor_name;
 
       this.session_date = this.note_selected.session_date? new Date(this.note_selected.session_date).toISOString(): '';
-      
+
       this.session_length_total = this.note_selected.session_length_total;
       this.session_length_total2 = this.note_selected.session_length_total2;
-      
+
       this.selectedValueTimeIn = this.note_selected.time_in;
-      
+
       this.selectedValueTimeOut = this.note_selected.time_in2;
       this.selectedValueTimeIn2 = this.note_selected.time_out;
       this.selectedValueTimeOut2 = this.note_selected.time_out2;
@@ -286,7 +290,7 @@ export class NoteBcbaEditComponent {
       // this.unitsAsignated = this.pa_assessmentsgroup.n_units;
       // console.log(this.pa_assessmentsgroup);
       this.cpt = this.pa_assessmentsgroup[0].cpt;
-      // console.log(this.cpt);  
+      // console.log(this.cpt);
     })
   }
 
@@ -299,7 +303,7 @@ export class NoteBcbaEditComponent {
     })
   }
 
-  
+
 
 
   specialistData(selectedValueInsurer){
@@ -314,9 +318,9 @@ export class NoteBcbaEditComponent {
   selectSpecialist(event:any){debugger
     event = this.selectedValueProviderName;
     this.specialistData(this.selectedValueProviderName);
-    
+
   }
- 
+
 
   hourTimeInSelected(value:number){
     this.selectedValueTimeIn = value;
@@ -328,64 +332,14 @@ export class NoteBcbaEditComponent {
 
   }
 
-  selectMaladaptive(behavior:any,i){
-    this.maladaptiveSelected = behavior;
-    this.caregivers_training_goals[i]= behavior
+  updateCaregiverGoal(index: number) {
+      console.log('Caregiver goal updated:', this.caregivers_training_goals[index]);
   }
 
-  addMaladaptive(maladaptiveSelected:any,i){
-    this.maladaptiveSelected = maladaptiveSelected;
-    this.caregivers_training_goals[i]= maladaptiveSelected
-
-    // this.caregivers_training_goals.push({
-    //   caregiver_goal: this.maladaptiveSelected.caregiver_goal,
-    //   porcent_of_correct_response: this.porcent_of_correct_response,
-    // })
-
-    //si existe un elemento actualiza ese elemento en la lista
-    if(this.caregivers_training_goals.length > 1){
-      this.caregivers_training_goals.splice(this.caregivers_training_goals,1);
-    }
-
-    this.maladaptiveSelected = null;
-    this.caregiver_goal = '';
-    this.porcent_of_correct_response = null;
+  updateRbtGoal(index: number) {
+      console.log('RBT goal updated:', this.rbt_training_goals[index]);
   }
 
-  selectReplacement(replacemen:any,i){
-    this.replacementSelected = replacemen;
-    this.rbt_training_goals[i]= replacemen;
-  }
-
-  addReplacement(replacementSelected:any,i){
-    this.replacementSelected = replacementSelected;
-    this.rbt_training_goals[i]= replacementSelected;
-
-    // this.rbt_training_goals.push({
-    //   lto: this.replacementSelected.lto,
-    //   porcent_of_correct_response: this.porcent_of_correct_response,
-    // })
-
-    //si existe un elemento actualiza ese elemento en la lista
-    if(this.rbt_training_goals.length > 1){
-      this.rbt_training_goals.splice(this.rbt_training_goals,1);
-    }
-    this.replacementSelected = null;
-    this.lto = '';
-    this.porcent_of_correct_response = null;
-  }
-
-  
-
-  back(){
-    this.replacementSelected = null;
-    this.maladaptiveSelected = null;
-    this.porcent_of_occurrences = null;
-    this.porcent_of_correct_response = null;
-  }
-
-
-  
 
   //funcion para la primera imagen.. funciona
   loadFile($event:any){
@@ -425,7 +379,7 @@ export class NoteBcbaEditComponent {
     event = this.selectedValueRBT;
     this.speciaFirmaDataRbt(this.selectedValueRBT);
     // console.log(this.selectedValueRBT);
-    
+
   }
 
   speciaFirmaDataBcba(selectedValueBCBA){
@@ -442,11 +396,11 @@ export class NoteBcbaEditComponent {
     event = this.selectedValueBCBA;
     this.speciaFirmaDataBcba(this.selectedValueBCBA);
     // console.log(this.selectedValueBCBA);
-    
-  }
-  
 
-  save(){debugger
+  }
+
+
+  save(){
     this.text_validation = '';
     // if(!this.name||!this.email ||!this.surname ){
     //   this.text_validation = 'Los campos con * son obligatorios';
@@ -457,7 +411,7 @@ export class NoteBcbaEditComponent {
     //   this.text_validation = 'Las contraseña debe ser igual';
     //   return;
     // }
-    
+
 
 
     let formData = new FormData();
@@ -468,6 +422,10 @@ export class NoteBcbaEditComponent {
     formData.append('location', this.location);
     formData.append('birth_date', this.birth_date);
     formData.append('session_date', this.session_date);
+
+    if (this.summary_note) {
+      formData.append('summary_note', this.summary_note);
+    }
 
     if(this.selectedValueTimeIn ){
       formData.append('time_in', this.selectedValueTimeIn+'' ? this.selectedValueTimeIn+'' : "0");
@@ -481,7 +439,7 @@ export class NoteBcbaEditComponent {
     if(this.selectedValueTimeOut2 ){
       formData.append('time_out2', this.selectedValueTimeOut2+''? this.selectedValueTimeOut2+'' : "0");
     }
-    
+
     if(this.selectedValueRendering ){
       formData.append('rendering_provider', this.selectedValueRendering);
     }
@@ -509,7 +467,7 @@ export class NoteBcbaEditComponent {
     if(this.caregivers_training_goals ){
       formData.append('caregiver_goals', JSON.stringify(this.caregivers_training_goals));
     }
-    
+
 
     if(this.FILE_SIGNATURE_RBT ){
       formData.append('imagen', this.FILE_SIGNATURE_RBT);
@@ -526,7 +484,7 @@ export class NoteBcbaEditComponent {
 
     this.noteBcbaService.editNote(formData,this.note_selectedId ).subscribe((resp:any)=>{
       // console.log(resp);
-      
+
       if(resp.message == 403){
         this.text_validation = resp.message_text;
       }else{
@@ -561,4 +519,82 @@ export class NoteBcbaEditComponent {
 //         return num1 * num2;
 //     }
 // }
+//
+  generateAISummary() {
+    if (!this.checkDataSufficient()) {
+        Swal.fire('Warning', 'Please fill all the required fields', 'warning');
+        return;
+    }
+    this.isGeneratingSummary = true;
+    const data = {
+        diagnosis: this.diagnosis_code,
+        birthDate: this.birth_date,
+        startTime: this.selectedValueTimeIn ? this.selectedValueTimeIn : null,
+        endTime: this.selectedValueTimeOut ? this.selectedValueTimeOut : null,
+        startTime2: this.selectedValueTimeIn2 ? this.selectedValueTimeIn2 : null,
+        endTime2: this.selectedValueTimeOut2 ? this.selectedValueTimeOut2 : null,
+        pos: this.getPos(this.meet_with_client_at),
+        caregiverGoals: this.caregivers_training_goals.map(g => ({
+            goal: g.caregiver_goal,
+            percentCorrect: g.porcent_of_correct_response
+        })),
+        rbtTrainingGoals: this.rbt_training_goals.map(g => ({
+            goal: g.lto,
+            percentCorrect: g.porcent_of_correct_response
+        })),
+        noteDescription: this.note_description
+    };
+
+    this.noteBcbaService.generateAISummary(data).subscribe(
+        (response: any) => {
+            this.summary_note = response.summary;
+            this.isGeneratingSummary = false;
+        },
+        (error) => {
+            console.error('Error generating AI summary:', error);
+            Swal.fire('Error', 'Error generating AI summary. Please try again.', 'error');
+            this.isGeneratingSummary = false;
+        }
+    );
+  }
+
+  checkDataSufficient(): boolean {
+    if (!this.note_selected) return false;
+
+    const hasTime = this.selectedValueTimeIn && this.selectedValueTimeOut;
+    if (!hasTime) return false;
+
+    if (!this.meet_with_client_at) return false;
+
+    if (!this.caregivers_training_goals || this.caregivers_training_goals.length === 0) return false;
+    const allCaregiverGoalsValid = this.caregivers_training_goals.every(g =>
+        g.caregiver_goal && (g.porcent_of_correct_response !== undefined && g.porcent_of_correct_response !== null)
+    );
+    if (!allCaregiverGoalsValid) return false;
+
+    if (!this.rbt_training_goals || this.rbt_training_goals.length === 0) return false;
+    const allRbtGoalsValid = this.rbt_training_goals.every(g =>
+        g.lto && (g.porcent_of_correct_response !== undefined && g.porcent_of_correct_response !== null)
+    );
+    if (!allRbtGoalsValid) return false;
+
+    if (!this.note_description) return false;
+
+    return true;
+  }
+
+  getPos(posCode: string) {
+    switch (posCode) {
+        case '03':
+            return 'School';
+        case '12':
+            return 'Home';
+        case '02':
+            return 'Telehealth';
+        case '99':
+            return 'Other';
+        default:
+            return 'Unknown';
+    }
+  }
 }
