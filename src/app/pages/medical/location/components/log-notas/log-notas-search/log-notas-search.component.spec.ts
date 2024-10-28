1f6746bd103c8507418c887988bdc65ba83f2381
Lord Zorel <marco.friz.p@gmail.com>
2024-10-28 10:54:15 -0300

    fixing test

--- src/app/pages/medical/location/components/log-notas/log-notas-search/log-notas-search.component.spec.ts
+++ src/app/pages/medical/location/components/log-notas/log-notas-search/log-notas-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogNotasSearchComponent } from './log-notas-search.component';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 fdescribe('LogNotasSearchComponent', () => {
   let component: LogNotasSearchComponent;
@@ -9,6 +11,7 @@ fdescribe('LogNotasSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LogNotasSearchComponent],
+      imports: [SharedModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LogNotasSearchComponent);
