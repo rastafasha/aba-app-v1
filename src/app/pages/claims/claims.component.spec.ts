32ecdf9215f5a4bac5a8137c0222dc05dc9a9a50
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-09 12:21:44 -0300

    fixed tests

--- src/app/pages/claims/claims.component.spec.ts
+++ src/app/pages/claims/claims.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ClaimsComponent } from './claims.component';
+import { SkeletonLoaderComponent } from 'src/app/shared/components/skeleton-loader/skeleton-loader.component';
 
 describe('ClaimsComponent', () => {
   let component: ClaimsComponent;
@@ -9,7 +10,7 @@ describe('ClaimsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
-      declarations: [ ClaimsComponent ]
+      declarations: [ ClaimsComponent, SkeletonLoaderComponent, ]
     })
     .compileComponents();
 
