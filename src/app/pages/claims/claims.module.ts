cecbe083f16fa1d70152ecb8944c37db923ec007
PsHohe <40927549+PsHohe@users.noreply.github.com>
2024-11-09 11:43:17 -0300

    removed wrong import

--- src/app/pages/claims/claims.module.ts
+++ src/app/pages/claims/claims.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { ClaimsRoutingModule } from './claims.routing';
 import { ClaimsComponent } from './claims.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { LayoutModule } from 'src/app/layout/layout.module';
 
 @NgModule({
   declarations: [ClaimsComponent],
-  imports: [CommonModule, ClaimsRoutingModule, SharedModule, LayoutModule],
+  imports: [CommonModule, ClaimsRoutingModule, SharedModule],
 })
 export class ClaimsModule {}
