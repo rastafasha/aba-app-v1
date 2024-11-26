b157c1e367976b181f0dc502ebe5550299d6f720
Lord Zorel <marco.friz.p@gmail.com>
2024-11-26 14:58:16 -0300

    fixing boolean problem

--- src/app/core/services/patients.v2.service.ts
+++ src/app/core/services/patients.v2.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { url_servicios } from 'src/app/config/config';
 import { PatientV2 } from '../models';
 import { RepositoryV2Service } from './repository.v2.service';
-import { StringOrNullOrUndefined } from 'src/app/shared/utils';
 
 @Injectable({ providedIn: 'root' })
 export class PatientsV2Service extends RepositoryV2Service<PatientV2> {
@@ -19,14 +18,10 @@ export class PatientsV2Service extends RepositoryV2Service<PatientV2> {
   override untransform(data: PatientV2): unknown {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const result: any = { ...data };
-    result.pay = StringOrNullOrUndefined(data.pay);
-    result.telehealth = StringOrNullOrUndefined(data.telehealth);
-    result.birth_date = !data.birth_date
-      ? null
-      : data.birth_date.toISOString().split('T')[0];
-    result.elegibility_date = !data.elegibility_date
-      ? null
-      : data.elegibility_date.toISOString().split('T')[0];
+    result.birth_date = data?.birth_date?.toISOString().split('T')[0];
+    result.elegibility_date = data?.elegibility_date
+      ?.toISOString()
+      .split('T')[0];
     delete result.created_at;
     delete result.updated_at;
     delete result.delete_at;
