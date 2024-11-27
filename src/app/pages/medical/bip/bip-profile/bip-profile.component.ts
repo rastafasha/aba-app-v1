5a764589971826e4232b219967f5f823aefa4880
rastafasha <mercadocreativo@gmail.com>
2024-11-27 15:01:24 -0400

    bip profile

--- src/app/pages/medical/bip/bip-profile/bip-profile.component.ts
+++ src/app/pages/medical/bip/bip-profile/bip-profile.component.ts
@@ -39,25 +39,30 @@ export class BipProfileComponent implements OnInit {
   documents = [];
   maladaptives = [];
   maladaptive_behavior = [];
-  assesstments = [];
   assesstmentsDocuments = [];
   prevalent_setting_event_and_atecedents = [];
   interventions: any;
 
+  attention :any = [];
+  tangibles :any = [];
+  escape :any = [];
+  sensory :any = [];
+  caregivers_training_goals :any = [];
+
   bip_selected: any = {};
   patient_selected: any = {};
-  reduction_goals = [];
+  reduction_goal :any = [];
   reduction_goals_goalltos = [];
   reduction_goals_goalstos = [];
   sustitution_goal = [];
   sustitution_goal_ltos: any[] = [];
   sustitution_goal_stos: any[] = [];
-  family_envolment: any[] = [];
+  family_envolment: any = [];
   monitoring_evalutating: any[] = [];
   monitoring_evalutating_goals: any[] = [];
   generalization_training: any[] = [];
   transition_fading_plans: any[] = [];
-  de_escalation_techniques: any[] = [];
+  
   analyst_signature: any = '';
   analyst_signature_date = '';
   parent_guardian_signature: any = '';
@@ -113,10 +118,7 @@ export class BipProfileComponent implements OnInit {
   hypothesis_based_intervention: any;
   assestmentEvaluationSettings: any;
 
-  accesstoTangibles: any;
-  accesstoAttention: any;
-  accesstoEscape: any;
-  accesstoSensory: any;
+  
 
   imagenSerUrl = environment.url_media;
 
@@ -162,155 +164,15 @@ export class BipProfileComponent implements OnInit {
       .subscribe((resp) => {
         console.log(resp);
         this.bip_selected = resp.bip;
+        this.maladaptives = resp.maladaptives;
         this.patient_selected = resp.patient ? resp.patient : null;
-        this.patientId = resp.patient.patient_id;
+        this.patientId = this.patient_selected.patient_id;
 
-        this.type_of_assessment = this.bip_selected.type_of_assessment;
 
-        this.documents = this.bip_selected.documents_reviewed;
-        console.log(this.documents);
-
-        (this.background_information =
-          this.bip_selected.background_information),
-          (this.previus_treatment_and_result =
-            this.bip_selected.previus_treatment_and_result),
-          (this.current_treatment_and_progress =
-            this.bip_selected.current_treatment_and_progress),
-          (this.education_status = this.bip_selected.education_status),
-          (this.phisical_and_medical_status =
-            this.bip_selected.phisical_and_medical_status),
-          (this.assestment_conducted = this.bip_selected.assestment_conducted),
-          
-        this.maladaptives = this.bip_selected.maladaptives;
-        this.maladaptive_behavior = this.bip_selected.maladaptives?.[0].title;
-
-        // console.log(this.maladaptives);
-        // console.log(this.maladaptive_behavior);
-
-        this.assesstments = this.bip_selected.assestment_conducted_options;
-        this.assesstmentsDocuments =
-          this.bip_selected.assestment_conducted_options;
-        this.prevalent_setting_event_and_atecedents =
-          this.bip_selected.prevalent_setting_event_and_atecedents;
-        // this.interventions =this.bip_selected.interventions;
-
-        this.analyst_signature =
-          this.bip_selected.consent_to_treatment.analyst_signature;
-        this.analyst_signature_date =
-          this.bip_selected.consent_to_treatment.analyst_signature_date;
-        this.parent_guardian_signature =
-          this.bip_selected.consent_to_treatment.parent_guardian_signature;
-
-        const imageData =
-          'data:image/png;base64,' + btoa(this.parent_guardian_signature);
-        // const imageData2 = 'data:image/png;base64,' + btoa(this.analyst_signature);
-        console.log(imageData);
-        // this.parent_guardian_signature = imageData;
-
-        this.parent_guardian_signature_date =
-          this.bip_selected.consent_to_treatment.parent_guardian_signature_date;
-        this.reduction_goals = this.bip_selected.reduction_goal;
-        this.reduction_goals_goalltos =
-          this.bip_selected.reduction_goal[0]?.goalltos;
-        this.reduction_goals_goalstos =
-          this.bip_selected.reduction_goal[0]?.goalstos;
-        this.sustitution_goal = this.bip_selected.sustitution_goal;
-        this.sustitution_goal_ltos =
-          this.bip_selected.sustitution_goal[0]?.goalltos;
-        this.sustitution_goal_stos =
-          this.bip_selected.sustitution_goal[0]?.goalstos;
-
-        this.family_envolment =
-          this.bip_selected.family_envolment[0]?.caregivers_training_goals;
-
-          console.log(Array.isArray(this.monitoring_evalutating_goals)); // Should log true if it's an array
-        this.monitoring_evalutating = this.bip_selected.monitoring_evalutating;
-        this.monitoring_evalutating_goals =
-          this.bip_selected.monitoring_evalutating[0]?.rbt_training_goals;
-          console.log(this.monitoring_evalutating_goals);
-        this.generalization_training =
-          this.bip_selected.generalization_training;
-        this.transition_fading_plans =
-          this.bip_selected.generalization_training[0]?.transition_fading_plans;
-        this.de_escalation_techniques =
-          this.bip_selected.de_escalation_technique[0]?.recomendation_lists;
-
-        this.hypothesis_based_intervention =
-          this.bip_selected.hypothesis_based_intervention;
-        this.assestmentEvaluationSettings =
-          this.bip_selected.assestmentEvaluationSettings;
-
-        this.accesstoTangibles = this.bip_selected.tangibles
-          ? this.bip_selected.tangibles
-          : null;
-        this.accesstoAttention = this.bip_selected.attention
-          ? this.bip_selected.attention
-          : null;
-        this.accesstoEscape = this.bip_selected.escape
-          ? this.bip_selected.escape
-          : null;
-        this.accesstoSensory = this.bip_selected.sensory
-          ? this.bip_selected.sensory
-          : null;
-
-        this.interventions = this.bip_selected.interventions
-          ? this.bip_selected.interventions
-          : null;
-
-        this.phiysical_and_medical = this.bip_selected.phiysical_and_medical;
-        this.phiysical_and_medical_status =
-          this.bip_selected.phiysical_and_medical_status;
-
-        this.caregiver_requirements_for_prevention_of_crisis =
-          this.bip_selected.crisis_plan[0]?.caregiver_requirements_for_prevention_of_crisis;
-        this.crisis_description =
-          this.bip_selected.crisis_plan[0]?.crisis_description;
-          // console.log(this.crisis_description);
-        this.crisis_note = this.bip_selected.crisis_plan[0]?.crisis_note;
-        this.crisis_plan = this.bip_selected.crisis_plan[0];
-        this.risk_factors = this.bip_selected.crisis_plan[0]?.risk_factors;
-        /*
-        const jsonObj =
-        JSON.parse(this.reduction_goals_goalstos.toString()) || '';
-        this.reduction_goals_goalstos = jsonObj;
-        // console.log(this.reduction_goals_goalstos);
-
-        const jsonObj1 =
-          JSON.parse(this.sustitution_goal_stos.toString()) || '';
-        this.sustitution_goal_stos = jsonObj1;
-        // console.log(this.sustitution_goal_stos);
-
-        const jsonObj2 =
-          JSON.parse(this.sustitution_goal_ltos.toString()) || '';
-        this.sustitution_goal_ltos = jsonObj2;
-        // console.log(this.sustitution_goal_ltos);
-
-        const jsonObj3 = JSON.parse(this.family_envolment.toString()) || '';
-        this.family_envolment = jsonObj3;
-        // console.log(this.family_envolment);
-
-        const jsonObj4 =
-          JSON.parse(this.monitoring_evalutating_goals.toString()) || '';
-        this.monitoring_evalutating_goals = jsonObj4;
-        // console.log(this.monitoring_evalutating_goals);
-
-        const jsonObj5 =
-          JSON.parse(this.transition_fading_plans.toString()) || '';
-        this.transition_fading_plans = jsonObj5;
-        // console.log(this.transition_fading_plans);
-
-        const jsonObj6 =
-          JSON.parse(this.de_escalation_techniques.toString()) || '';
-        this.de_escalation_techniques = jsonObj6;
-        // console.log(this.transition_fading_plans);
-
-        const jsonObj7 = JSON.parse(this.risk_factors) || '';
-        this.risk_factors = jsonObj7;
-        // console.log(this.risk_factors);
-            */
-        this.other = this.risk_factors?.[0].other;
+        
         // console.log(this.other);
-       this.do_not_apply = this.risk_factors[0]?.do_not_apply;
+
+       /*this.do_not_apply = this.risk_factors[0]?.do_not_apply;
         this.elopement = this.risk_factors[0]?.elopement;
         this.assaultive_behavior = this.risk_factors[0]?.assaultive_behavior;
         this.aggression = this.risk_factors[0]?.aggression;
@@ -337,7 +199,6 @@ export class BipProfileComponent implements OnInit {
         this.suicidality_added = resp.bip.crisis_plan[0]?.suicidalities;
         const jsonObj8 = JSON.parse(this.suicidality_added) || '';
         this.suicidality_added = jsonObj8;
-        // console.log(this.suicidality_added);
 
         this.not_present = this.suicidality_added[0]?.not_present;
         this.ideation = this.suicidality_added[0]?.ideation;
@@ -348,7 +209,6 @@ export class BipProfileComponent implements OnInit {
         this.homicidality_added = resp.bip.crisis_plan[0]?.homicidalities;
         const jsonObj9 = JSON.parse(this.homicidality_added) || '';
         this.homicidality_added = jsonObj9;
-        // console.log(this.homicidality_added);
 
         this.not_present_homicidality =
           this.homicidality_added[0]?.not_present_homicidality;
@@ -357,7 +217,7 @@ export class BipProfileComponent implements OnInit {
         this.plan_homicidality = this.homicidality_added[0]?.plan_homicidality;
         this.means_homicidality = this.homicidality_added[0]?.means_homicidality;
         this.prior_attempt_homicidality =
-          this.homicidality_added[0]?.prior_attempt_homicidality;
+          this.homicidality_added[0]?.prior_attempt_homicidality;*/
       });
   }
 
