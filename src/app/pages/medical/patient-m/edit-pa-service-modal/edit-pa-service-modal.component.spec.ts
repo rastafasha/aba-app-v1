fa5bda4681947fd5fa24da66d6b5ae77b2adbfd4
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2024-12-12 23:21:29 -0400

    arreglo en paciente fixed3

--- src/app/pages/medical/patient-m/edit-pa-service-modal/edit-pa-service-modal.component.spec.ts
+++ src/app/pages/medical/patient-m/edit-pa-service-modal/edit-pa-service-modal.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditPaServiceModalComponent } from './edit-pa-service-modal.component';
 import { MaterialModule } from 'src/app/shared/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('EditPaServiceModalComponent', () => {
   let component: EditPaServiceModalComponent;
@@ -15,7 +15,8 @@ describe('EditPaServiceModalComponent', () => {
       declarations: [ EditPaServiceModalComponent ],
       imports: [ MaterialModule, SharedModule ],
       providers: [
-        { provide: MatDialogRef, useValue: {} } // Provide a mock for MatDialogRef
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} } 
       ]
     })
     .compileComponents();
