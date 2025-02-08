a808d995a564e3179c4e1fa681d7b5dcc52cd71f
PsHohe <40927549+PsHohe@users.noreply.github.com>
2025-02-08 14:48:06 -0300

    refactor: Implement split-table component in interventions and replacements views
    - Replace static table implementation with reusable app-split-table component
    - Simplify template logic for interventions and replacements protocols
    - Improve code readability and consistency across components

--- src/app/pages/medical/notes-bcba/components/replacements2-vew.component.ts
+++ src/app/pages/medical/notes-bcba/components/replacements2-vew.component.ts
@@ -4,45 +4,36 @@ import { PlanProtocol } from 'src/app/core/models/notes.model';
 @Component({
   selector: 'app-replacements2-view',
   template: `
-    <!-- Interventions -->
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title">Replacements Protocols</h4>
-      </div>
-      <!-- body card -->
-      <div class="card-body p-0">
-      <div class="table-responsive content-box">
-          <table class="table mb-0 ">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Assessed</th>
-                <th>Modified</th>
-              </tr>
-            </thead>
-            <tbody>
-            <tr *ngFor="let replace of obj_inprogress; let i = index">
-              <td>{{ replace.name }}</td>
-              <td>
-              <i  *ngIf="replace.assessed === true"><i class="fa fa-check"  ></i></i>
-              <i  *ngIf="replace.assessed === false"><i class="fa fa-times"  ></i></i>
-              </td>
-              <td>
-              <i  *ngIf="replace.modified === true"><i class="fa fa-check"  ></i></i>
-              <i  *ngIf="replace.modified === false"><i class="fa fa-times"  ></i></i>
-              </td>
+    <div class="card-header">
+      <h4 class="card-title">Replacements Protocols</h4>
+    </div>
+    <div class="card-body p-0">
+      <app-split-table [data]="obj_inprogress" [headerTemplate]="replacementsHeader" [rowTemplate]="replacementsRow">
+        <ng-template #replacementsHeader>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Assessed</th>
+              <th>Modified</th>
             </tr>
-          </tbody>
-          </table>
-        </div>
+          </thead>
+        </ng-template>
+
+        <ng-template #replacementsRow let-replace>
+          <td>{{ replace.name }}</td>
+          <td>
+            <i *ngIf="replace.assessed" class="fa fa-check"></i>
+            <i *ngIf="!replace.assessed" class="fa fa-times"></i>
+          </td>
+          <td>
+            <i *ngIf="replace.modified" class="fa fa-check"></i>
+            <i *ngIf="!replace.modified" class="fa fa-times"></i>
+          </td>
+        </ng-template>
+      </app-split-table>
     </div>
-    </div>
-    <!-- interventions -->
   `,
 })
 export class Replacement2ViewComponent {
   @Input() obj_inprogress: PlanProtocol[];
-
-
-
 }
