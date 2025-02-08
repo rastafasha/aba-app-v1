cc6c49585e884c174f05de5db0c948a665e98463
PsHohe <40927549+PsHohe@users.noreply.github.com>
2025-02-08 14:09:48 -0300

    feat: Add card-with-boolean component

---
+++ src/app/shared/components/card-with-boolean/card-with-boolean.component.ts
@@ -0,0 +1,29 @@
+
+// Create a component for Angular with what follows: AI!
+
+
+<div class="card">
+                    <div class="card-header">
+                      <!-- <h4 class="card-title">Modifications needed at this time</h4> -->
+                    </div>
+                    <div class="card-body p-0">
+                      <div class="personal-list-out">
+                        <div class="container">
+                          <div class="col-xs-12">
+                            <div class="row">
+                              <div class="col-xs-12 col-sm-4">
+                                <div class="d-flex justify-content-between align-items-center">
+                                  <span>Modifications needed at this time</span>
+                                  <div class="d-flex justify-content-between align-items-center">
+                                    <i *ngIf="note_selected?.modifications_needed_at_this_time" class="fa fa-check"></i>
+                                    <i *ngIf="!note_selected?.modifications_needed_at_this_time"
+                                      class="fa fa-times"></i>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
